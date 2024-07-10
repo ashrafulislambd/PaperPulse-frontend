a9e585aff5be1017354adbead35a72bd347b816d
Md. Ashraful Islam <imdashraful17@gmail.com>
2024-07-10 15:13:30 +0600

    Fixed a little bug on register page NextRouter was not mounted

--- app/register/page.js
+++ app/register/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../config";
 import { isLoggedIn } from "../auth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
     const [username, setUsername] = useState("");
